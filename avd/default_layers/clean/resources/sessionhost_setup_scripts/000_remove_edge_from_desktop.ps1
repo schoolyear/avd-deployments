2f2e48f23510115298b52532243ebf6146f9e2a8
Laurens Bremers <laurens@bremersklaver.nl>
2024-09-04 08:54:40 +0200

    added note about edge script

--- avd/default_layers/clean/resources/sessionhost_setup_scripts/000_remove_edge_from_desktop.ps1
+++ avd/default_layers/clean/resources/sessionhost_setup_scripts/000_remove_edge_from_desktop.ps1
@@ -1,3 +1,4 @@
+# todo: cleanup, this doesn't work
 # Remove Microsoft Edge link so don't student don't try using that browser
 Write-Host "Checking for microsoft edge link"
 $edgeLink = "C:\Users\Public\Desktop\Microsoft Edge.lnk"
