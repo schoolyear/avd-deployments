cb34c116f565f53ab3735b64b29614ecbd62056b
Laurens Bremers <laurens@bremersklaver.nl>
2024-09-04 08:52:57 +0200

    proxy layer

--- avd/default_layers/proxy/resources/sessionhost_setup_scripts/000_system_proxy_setup.ps1
+++ avd/default_layers/proxy/resources/sessionhost_setup_scripts/000_system_proxy_setup.ps1
@@ -1,44 +1,79 @@
-$url = "http://169.254.169.254/metadata/instance/compute/tagsList?api-version=2021-02-01"
-$headers = @{
-    "Metadata" = "true"
-}
+$ErrorActionPreference = "Stop"
 
-try
+function GetProxyIPsString()
 {
-    # Make the request and get the response
-    $response = Invoke-RestMethod -Uri $url -Headers $headers -Method Get
-}
-catch
-{
-    Write-Error "Could not make request to metadata endpoint: $_"
-    exit 1
-}
-
-# Find the "name": "proxyVmIpAddr" and print its value
-$found = $false
-$proxyIpAddr = ""
-foreach ($tag in $response)
-{
-    if ($tag.name -eq "proxyVmIpAddr")
-    {
-        $proxyIpAddr = $tag.value
-        $found = $true
-        break
+    $url = "http://169.254.169.254/metadata/instance/compute/tagsList?api-version=2021-02-01"
+    $headers = @{
+        "Metadata" = "true"
     }
+
+    try
+    {
+        # Make the request and get the response
+        $response = Invoke-RestMethod -Uri $url -Headers $headers -Method Get
+    }
+    catch
+    {
+        Write-Error "Could not make request to metadata endpoint: $_"
+        exit 1
+    }
+
+    # Find the "name": "proxyVmIpAddr" and print its value
+    $found = $false
+    $proxyIpAddr = ""
+    foreach ($tag in $response)
+    {
+        if ($tag.name -eq "proxyVmIpAddr")
+        {
+            $proxyIpAddr = $tag.value
+            $found = $true
+            break
+        }
+    }
+
+    if (!$found)
+    {
+        Write-Error "Could not find proxyVmIpAddr in metadata"
+        exit 1
+    }
+
+    return $proxyIPAddr
 }
 
-if (!$found)
+$proxyIP = GetProxyIPsString
+$proxyString = (($proxyIP -split ',') | ForEach-Object { "PROXY $_" }) -join '; '
+
+$whitelist = Get-Content -Path "C:\domainwhitelist.json" | ConvertFrom-Json
+$domainChecks = 'false'
+if ($whitelist.PSObject.Properties.Name.count -gt 0)
 {
-    Write-Error "Could not find proxyVmIpAddr in metadata"
-    exit 1
+    $domainChecks = ($whitelist.PSObject.Properties | ForEach-Object {
+        "dnsDomainIs(host, `"$( $_.Name )`")"
+    }) -join ' || '
 }
 
-# todo: make this dynamic
+$pacScriptPath = "C:\static\proxy.pac"
+$pacSCriptParentPath = Split-Path -Path $pacScriptPath -Parent
+
+if (-Not (Test-Path $pacSCriptParentPath))
+{
+    New-Item -ItemType Directory -Force -Path $pacSCriptParentPath
+}
+
+@"
+function FindProxyForURL(url, host) {
+    if (!dnsDomainIs(host, 'wvd.microsoft.com') && ($domainChecks)) {
+        return "$( $proxyString )";
+    }
+    return "DIRECT";
+}
+"@ | Out-File -FilePath $pacScriptPath
+
 try
 {
     Write-Host "Setting system wide proxy..."
-    bitsadmin /util /setieproxy LOCALSYSTEM MANUAL_PROXY "$( $proxyIpAddr ):8080" *.wvd.microsoft.com
-    bitsadmin /util /setieproxy NETWORKSERVICE MANUAL_PROXY "$( $proxyIpAddr ):8080" *.wvd.microsoft.com
+    bitsadmin /util /setieproxy LOCALSYSTEM AUTOSCRIPT "http://localhost:2015/proxy.pac"
+    bitsadmin /util /setieproxy NETWORKSERVICE AUTOSCRIPT "http://localhost:2015/proxy.pac"
 }
 catch
 {
