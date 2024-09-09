04cd7e7690d183719a27f8b589b34d462f6acbf3
Laurens Bremers <laurens@bremersklaver.nl>
2024-09-09 11:39:20 +0200

    whitelist SY for all environments

--- default_layers/vdi_browser/resources/sessionhost_setup_scripts/000_post_firewall_whitelist.ps1
+++ default_layers/vdi_browser/resources/sessionhost_setup_scripts/000_post_firewall_whitelist.ps1
@@ -1,13 +1,18 @@
-# todo: edit for other environments
-$syExamsFolder = "C:\Program Files (x86)\Schoolyear\Schoolyear Browser Development (confidential)"
-$syExamsExe = "$syExamsFolder\shell\Schoolyear Exams.exe"
-$syVDIServiceExe = "$syExamsFolder\schoolyear-vdi-service.exe"
-$avdTokenProviderExe = "$syExamsFolder\shell\resources\avd_token_provider.exe"
+$syFolder = "C:\Program Files (x86)\Schoolyear\"
+$installationFolders = @("Schoolyear Browser Development (confidential)", "Schoolyear Browser Testing", "Schoolyear Browser Preview", "Schoolyear Browser")
 
-# Allow the VDI browser
-New-NetFirewallRule -DisplayName "Allow Schoolyear Browser outbound" -Program $syExamsExe -Direction Outbound -Action Allow -Profile Any | Out-Null
-# Allow the background service
-New-NetFirewallRule -DisplayName "Allow Schoolyear VDI service" -Program $syVDIServiceExe -Direction Outbound -Action Allow -Profile Any | Out-Null
-# Allow the token provider
-New-NetFirewallRule -DisplayName "Allow AVD token provider" -Program $avdTokenProviderExe -Direction Outbound -Action Allow -Profile Any | Out-Null
+foreach ($folder in $installationFolders) {
+    $syExamsFolder= Join-Path -Path $syFolder -ChildPath $installationFolders
+
+    $syExamsExe = "$syExamsFolder\shell\Schoolyear Exams.exe"
+    $syVDIServiceExe = "$syExamsFolder\schoolyear-vdi-service.exe"
+    $avdTokenProviderExe = "$syExamsFolder\shell\resources\avd_token_provider.exe"
+
+    # Allow the VDI browser
+    New-NetFirewallRule -DisplayName "Allow Schoolyear Browser outbound" -Program $syExamsExe -Direction Outbound -Action Allow -Profile Any | Out-Null
+    # Allow the background service
+    New-NetFirewallRule -DisplayName "Allow Schoolyear VDI service" -Program $syVDIServiceExe -Direction Outbound -Action Allow -Profile Any | Out-Null
+    # Allow the token provider
+    New-NetFirewallRule -DisplayName "Allow AVD token provider" -Program $avdTokenProviderExe -Direction Outbound -Action Allow -Profile Any | Out-Null
+}
 
