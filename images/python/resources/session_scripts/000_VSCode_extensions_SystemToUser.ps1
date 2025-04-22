65349f6d797a7b97838cfdf84d05ed212b6309f7
Nunen <d.j.v.nunen@tue.nl>
2025-04-22 14:18:13 +0200

    added debugging logging to session script

--- images/python/resources/session_scripts/000_VSCode_extensions_SystemToUser.ps1
+++ images/python/resources/session_scripts/000_VSCode_extensions_SystemToUser.ps1
@@ -1,4 +1,4 @@
-Param ($username)
+Param ($username, $homedir)
 
 # This file is a placeholder and can be removed
 # The files in this folder (000_name.ps1) are executed for each user session starting on a session host
@@ -9,7 +9,14 @@ Param ($username)
 $sourcePath = "C:\Windows\System32\config\systemprofile\.vscode\extensions"
 
 # Set the destination path
-$destinationPath = "C:\Users\$username\.vscode\"
+$destinationPath = "$homedir\.vscode\"
 
+# Log variables to file for debugging
+$username | Out-File -Append -FilePath C:\temp\SessionScriptVars.log
+$homedir | Out-File -Append -FilePath C:\temp\SessionScriptVars.log
+$sourcePath | Out-File -Append -FilePath C:\temp\SessionScriptVars.log
+$destinationPath | Out-File -Append -FilePath C:\temp\SessionScriptVars.log
+# Log content of source folder to file for debugging
+Get-ChildItem C:\Windows\System32\config\systemprofile\.vscode\extensions | Out-File -Append -FilePath C:\temp\SessionScriptVars.log
 # Copy the contents recursively, preserving structure
 Copy-Item -Path "$sourcePath" -Destination $destinationPath -Recurse -Force
