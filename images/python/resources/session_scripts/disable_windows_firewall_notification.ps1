d36d7f4b3bbaf203154761eda1e6ed2ecb3dc553
Nunen <d.j.v.nunen@tue.nl>
2025-05-26 11:00:01 +0200

    just an little change of comments

--- images/python/resources/session_scripts/disable_windows_firewall_notification.ps1
+++ images/python/resources/session_scripts/disable_windows_firewall_notification.ps1
@@ -2,5 +2,5 @@
 # The files are executed from the SYSTEM account and have admin priviledges
 # All files must exit without an error for the VDI Browser to start up properly
 
-#The line below disables the "Windows firewall has blockes some features of this app" pop-up
+#The line below disables the "Windows firewall has blockes some features of this app" pop-up as it pops up when running code the first time, because VSCode tries to access the internet.
 Set-NetFirewallProfile -Profile Domain,Private,Public -NotifyOnListen False
